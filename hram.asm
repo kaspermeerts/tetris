4e600d4dfee818ecd5eeef80d7cdfb6a478cbd2e
Kasper Meerts <kaspermeerts@gmail.com>
2021-04-01 00:35:05 +0200

    States 06,07,08,24,25,35 and various other routines

--- hram.asm
+++ hram.asm
@@ -6,11 +6,69 @@ hJoyHeld:: ; Currently pressed keys
 hJoyPressed:: ; Newly pressed keys
     db
 
-ds $FFB6 - $FF82
+ds $FF85 - $FF82
 
-hDMARoutine:: ds $C
+hVBlankInterruptTriggered:
+    db
 
-ds $FFE2 - ($FFB6 + $C)
+ds $FF9E - $FF86
+
+hLinesLeft::
+    db
+
+ds $FFA6 - $FF9F
+
+hTimer1:: ; Frame based timer
+    db
+hTimer2:: ; Unused?
+    db
+
+hFFA8:: db
+
+hLevel::
+    db
+
+ds $FFB6 - $FFAA
+
+hDMARoutine::
+    ds $A
+
+hGameType::
+    db
+
+hMusicType::
+    db
+
+hTypeALevel::
+    db
+
+hTypeBLevel::
+    db
+
+hTypeBHigh::
+    db
+
+hIsMultiplayer::
+    db
+
+ds $FFCC - $FFC6
+
+hSerialInterruptTriggered::
+    db
+
+ds $FFE1 - $FFCD
+
+hGameState:: db
 
 ; Incremented every VBlank
 hFrameCounter:: db
+
+hFFE3:: db
+
+hDemoNumber::
+    db
+
+ds $FFF4 - $FFE5
+
+hHeartMode:: ; Heart mode. Get it?
+    db
--- tetris.asm
+++ tetris.asm
@@ -1,4 +1,5 @@
 INCLUDE "hardware.inc"
+INCLUDE "charmap.asm"
 
 ; RST vectors (unused?)
 SECTION "RST 0", ROM0[$0000]
@@ -46,61 +47,62 @@ _Serial::
     push bc
     call SerialTableJump; To prepare the return address for the upcoming RST $28
     ld a, 1
-    ldh [$CC], a
+    ldh [hSerialInterruptTriggered], a
     pop bc
     pop de
     pop hl
     pop af
     reti
+
 SerialTableJump::
     ldh a, [$CD]
     rst $28             ; TableJump
-    dw Label_78
-    dw Label_9F
-    dw Label_A4
-    dw Label_BA
+    dw Call_78
+    dw Call_9F
+    dw Call_A4
+    dw Call_BA
     dw $27EA            ; XXX Is this used? The target is a simple ret
 
-Label_78:
-    ldh a, [$E1]
+Call_78:
+    ldh a, [hGameState]
     cp a, 7
-    jr z, Label_86
+    jr z, .label_86
     cp a, 6
     ret z
-    ld a, 6
-    ldh [$E1], a
+    ld a, 6             ; If not at or going to the title screen, go back to
+    ldh [hGameState], a ; the title screen
     ret
 
-Label_86:
+.label_86:
     ldh a, [rSB]
     cp a, $55
-    jr nz, Label_94
+    jr nz, .label_94
     ld a, $29
     ldh [$CB], a
     ld a, 1
-    jr Label_9C
-Label_94:
+    jr .label_9C
+
+.label_94:
     cp a, $29
     ret nz
     ld a, $55
     ldh [$CB], a
     xor a
-Label_9C:
+.label_9C:
     ldh [rSC], a
     ret
 
-Label_9F:
+Call_9F:
     ldh a, [rSB]
     ldh [$D0], a
     ret
 
-Label_A4:
+Call_A4:
     ldh a, [rSB]
     ldh [$D0], a
     ldh a, [$CB]
     cp a, $29
     ret z
-Label_AD:
     ldh a, [$CF]
     ldh [rSB], a
     ld a, $FF
@@ -109,7 +111,7 @@ Label_AD:
     ldh [rSC], a
     ret
 
-Label_BA:
+Call_BA:
     ldh a, [rSB]
     ldh [$D0], a
     ldh a, [$CB]
@@ -118,7 +120,7 @@ Label_BA:
     ldh a, [$CF]
     ldh [rSB], a
     ei
-    call $A98
+    call DelayMillisecond
     ld a, $80
     ldh [rSC], a
     ret
@@ -147,7 +149,7 @@ _Start::
 ; This is the exact same routine as in Super Mario Land
 ; XXX Is this unused?
 LookupTile::
-    call $29E3
+    call Call_29E3
 .wait1
     ldh a, [rSTAT]
     and a, STATF_VBL | STATF_OAM
@@ -223,7 +225,7 @@ Label_199:
     call $22AD
     call $229E
     call $1ED7
-    call $FFB6
+    call hDMARoutine
     call $18CA
     ld a, [$C0CE]       ; Score needs updating?
     and a
@@ -246,7 +248,7 @@ Label_1FB:
     ldh [rSCX], a
     ldh [rSCY], a
     inc a
-    ldh [$85], a
+    ldh [hVBlankInterruptTriggered], a
     pop hl
     pop de
     pop bc
@@ -355,17 +357,17 @@ Init::
     dec b
     jr nz, .copyDMAroutine
 
-    call ClearBgMap
+    call ClearTilemap9800
     call $7FF3          ; Init music?
 
     ld a, IEF_SERIAL | IEF_VBLANK
     ldh [rIE], a
     ld a, $37
-    ldh [$C0], a
+    ldh [hGameType], a
     ld a, $1C
-    ldh [$C1], a
+    ldh [hMusicType], a
     ld a, $24
-    ldh [$E1], a
+    ldh [hGameState], a
     ld a, LCDCF_ON
     ldh [rLCDC], a
     ei
@@ -381,100 +383,524 @@ MainLoop::
     call $7FF0          ; Update sound
 
     ldh a, [hJoyHeld]
-    and a, $0F
-    cp a, $0F           ; Reset if all buttons are pressed
+    and a, PADF_START | PADF_SELECT | PADF_B | PADF_A
+    cp a, PADF_START | PADF_SELECT | PADF_B | PADF_A
     jp z, Init.softReset
-    ld hl, $FFA6
+    ld hl, hTimer1
     ld b, 2
-.label_2DB
+.decrementTimer
     ld a, [hl]
     and a
-    jr z, $2E0
+    jr z, .skip
     dec [hl]
+.skip
     inc l
     dec b
-    jr nz, .label_2DB
-    ldh a, [$C5]
+    jr nz, .decrementTimer
+    ldh a, [hIsMultiplayer]
     and a
-    jr z, .label_2ED
+    jr z, .wait
     ld a, IEF_SERIAL | IEF_VBLANK
     ldh [rIE], a
-.label_2ED
-    ldh a, [$85]
+.wait
+    ldh a, [hVBlankInterruptTriggered]
     and a
-    jr z, .label_2ED
+    jr z, .wait
     xor a
-    ldh [$85], a
+    ldh [hVBlankInterruptTriggered], a
     jp MainLoop
+
 .dispatch
-    ldh a, [$E1]
+    ldh a, [hGameState]
     rst $28
 
-dw $1BCE
-dw $1CE2
-dw $1244
-dw $127B
-dw $1D06
-dw $1D26
-dw $03AE
-dw $0479
-dw $1444
-dw $148C
-dw $1A07
-dw $1DC0
-dw $1F16
-dw $1F1F
-dw $1525
-dw $14B0
-dw $157B
-dw $15BF
-dw $1629
-dw $167A
-dw $16EB
-dw $1913
-dw $0677
-dw $072C
-dw $0825
-dw $08E4
-dw $0B31
-dw $0CEB
-dw $0AD2
-dw $0D32
-dw $0E23
-dw $1112
-dw $0D99
-dw $0E8A
-dw $1DCE
-dw $1E41
-dw $0369
-dw $0393
-dw $1167
-dw $11E6
-dw $11FC
-dw $121C
-dw $05C7
-dw $05F7
-dw $12B3
-dw $1305
-dw $1324
-dw $1351
-dw $1367
-dw $137E
-dw $13B5
-dw $13E5
-dw $131B
-dw $03A0
-dw $27EA
+dw $1BCE ; 0x00
+dw $1CE2 ; 0x01
+dw $1244 ; 0x02
+dw $127B ; 0x03
+dw $1D06 ; 0x04
+dw $1D26 ; 0x05
+dw GameState_06 ; Load title screen
+dw GameState_07 ; Title screen
+dw GameState_08 ; Load Game Type/Music Type Selection Screen
+dw $148C ; 0x09
+dw $1A07 ; 0x0A
+dw $1DC0 ; 0x0B
+dw $1F16 ; 0x0C
+dw $1F1F ; 0x0D
+dw $1525 ; 0x0E
+dw $14B0 ; 0x0F
+dw $157B ; 0x10
+dw $15BF ; 0x11
+dw $1629 ; 0x12
+dw $167A ; 0x13
+dw $16EB ; 0x14
+dw $1913 ; 0x15
+dw $0677 ; 0x16
+dw $072C ; 0x17
+dw $0825 ; 0x18
+dw $08E4 ; 0x19
+dw $0B31 ; 0x1A
+dw $0CEB ; 0x1B
+dw $0AD2 ; 0x1C
+dw $0D32 ; 0x1D
+dw $0E23 ; 0x1E
+dw $1112 ; 0x1F
+dw $0D99 ; 0x20
+dw $0E8A ; 0x21
+dw $1DCE ; 0x22
+dw $1E41 ; 0x23
+dw GameState_24 ; Load the copyright screen
+dw GameState_25 ; Copyright screen
+dw $1167 ; 0x26
+dw $11E6 ; 0x27
+dw $11FC ; 0x28
+dw $121C ; 0x29
+dw $05C7 ; 0x2A
+dw $05F7 ; 0x2B
+dw $12B3 ; 0x2C
+dw $1305 ; 0x2D
+dw $1324 ; 0x2E
+dw $1351 ; 0x2F
+dw $1367 ; 0x30
+dw $137E ; 0x31
+dw $13B5 ; 0x32
+dw $13E5 ; 0x33
+dw $131B ; 0x34
+dw GameState_35 ; Copyright screen, but skippable
+dw $27EA ; 0x36
 
-Call_369::
+GameState_24::
+    call DisableLCD
+    call LoadCopyrightScreenTileset
+    ld de, CopyrightScreenTilemap
+    call LoadTilemap9800
+    call ClearSprites
+    ld hl, $C300        ; Demo data
+    ld de, $6450
+.loop
+    ld a, [de]
+    ldi [hl], a
+    inc de
+    ld a, h
+    cp a, $C4
+    jr nz, .loop
+    ld a, $D3           ; Turn on LCD, background and sprites
+    ldh [rLCDC], a
+    ld a, 4 * 60 + 10   ; 250 frames, 4⅙ seconds (TODO constant)
+    ldh [hTimer1], a
+    ld a, $25
+    ldh [hGameState], a
+    ret
 
-INCBIN "baserom.gb", $369, $2795 - $369
+GameState_25::
+    ldh a, [hTimer1]
+    and a
+    ret nz
+    ld a, 4 * 60 + 10   ; another 4 seconds TODO
+    ldh [hTimer1], a
+    ld a, $35
+    ldh [hGameState], a
+    ret
 
-ClearBgMap::
+GameState_35::
+    ldh a, [hJoyPressed]
+    and a
+    jr nz, .skip
+    ldh a, [hTimer1]
+    and a
+    ret nz
+.skip
+    ld a, $06
+    ldh [hGameState], a
+    ret
+
+GameState_06::
+    call DisableLCD
+    xor a
+    ldh [$E9], a
+    ldh [$98], a
+    ldh [$9C], a
+    ldh [$9B], a
+    ldh [$FB], a
+    ldh [$9F], a
+    ldh [$E3], a
+    ldh [$C7], a
+    call $2293
+    call $2651
+    call LoadCopyrightScreenTileset
+    ld hl, $C800
+.loop1
+    ld a, $2F
+    ldi [hl], a
+    ld a, h
+    cp a, $CC
+    jr nz, .loop1
+    ld hl, $C801
+    call $26A9
+    ld hl, $C80C
+    call $26A9
+    ld hl, $CA41
+    ld b, $0C
+    ld a, $8E
+.loop2
+    ldi [hl], a
+    dec b
+    jr nz, .loop2
+    ld de, $4B6F
+    call LoadTilemap9800
+    call ClearSprites
+    ld hl, wOAMBuffer
+    ld [hl], $80        ; TODO sets up the cursor sprite
+    inc l
+    ld [hl], $10
+    inc l
+    ld [hl], $58
+    ld a, $03
+    ld [$DFE8], a
+    ld a, $D3           ; TODO
+    ldh [rLCDC], a
+    ld a, $07
+    ldh [hGameState], a
+    ld a, $7D
+    ldh [hTimer1], a
+    ld a, $04
+    ldh [$C6], a
+    ldh  a, [$FFE4]
+    and a
+    ret nz
+    ld a, $13
+    ldh [$C6], a
+    ret
+
+StartDemo::
+    ld a, $37
+    ldh [hGameType], a  ; TODO 37 = A-Type, 77 = B-Type
+    ld a, 9             ; Demos go fast
+    ldh [hTypeALevel], a
+    xor a
+    ldh [hIsMultiplayer], a
+    ldh [$B0], a
+    ldh [$ED], a
+    ldh [$EA], a
+    ld a, $62
+    ldh [$EB], a
+    ld a, $B0
+    ldh [$EC], a
+    ldh a, [hDemoNumber]
+    cp a, 2
+    ld a, 2
+    jr nz, .skip        ; First load demo 2, then demo 1
+    ld a, $77           ; B-Type
+    ldh [hGameType], a
+    ld a, 9
+    ldh [hTypeBLevel], a
+    ld a, 2
+    ldh [hTypeBHigh], a
+    ld a, $63
+    ldh [$EB], a        ; Some type of random seed?
+    ld a, $B0
+    ldh [$EC], a
+    ld a, $11
+    ldh [$B0], a        ; Number of blocks played, probably important for determinism
+    ld a, 1
+.skip
+    ldh [hDemoNumber], a
+    ld a, $0A
+    ldh [hGameState], a
+    call DisableLCD
+    call Call_27AD
+    ld de, $4CD7
+    call LoadTilemap9800
+    call ClearSprites
+    ld a, $D3           ; TODO
+    ldh [rLCDC], a
+    ret
+
+; Unused?
+Call_474::
+    ld a, $FF
+    ldh [$E9], a
+    ret
+
+GameState_07::
+    ldh a, [hTimer1]
+    and a
+    jr nz, .skip
+    ld hl, $FFC6
+    dec [hl]
+    jr z, StartDemo
+    ld a, 125           ; 2 and one twelfth of a second
+    ldh [hTimer1], a
+.skip
+    call DelayMillisecond
+    ld a, $55
+    ldh [rSB], a
+    ld a, $80
+    ldh [rSC], a
+    ldh a, [hSerialInterruptTriggered]
+    and a
+    jr z, Label_4A2
+    ldh a, [$CB]
+    and a
+    jr nz, Label_4A2.launchMultiplayer
+    xor a
+    ldh [hSerialInterruptTriggered], a
+    jr UpdateCursor.initCursor
+
+Label_4A2:
+    ldh a, [hJoyPressed]
+    ld b, a
+    ldh a, [hIsMultiplayer] ; (Ab)used here to keep track of the pointer
+    bit 2, b                ; Select TODO
+    jr nz, UpdateCursor.selectPressed
+    bit 4, b                ; Right
+    jr nz, UpdateCursor.rightPressed
+    bit 5, b                ; Left
+    jr nz, UpdateCursor.leftPressed
+    bit 3, b                ; Start
+    ret z
+    and a
+    ld a, $08
+    jr z, .startPressed     ; Launch game
+    ld a, b
+    cp a, 8
+    ret nz
+    ldh a, [$CB]
+    cp a, $29
+    jr z, .launchMultiplayer
+    ld a, $29
+    ldh [rSB], a
+    ld a, $81
+    ldh [rSC], a
+.wait
+    ldh a, [hSerialInterruptTriggered]
+    and a
+    jr z, .wait
+    ldh a, [$CB]
+    and a
+    jr z, UpdateCursor.initCursor
+.launchMultiplayer
+    ld a, $2A
+.nextState
+    ldh [hGameState], a
+    xor a
+    ldh [hTimer1], a
+    ldh [hTypeALevel], a
+    ldh [hTypeBLevel], a
+    ldh [hTypeBHigh], a
+    ldh [hDemoNumber], a
+    ret
+
+.startPressed
+    push af
+    ldh a, [hJoyHeld]
+    bit 7, a            ; Down button. TODO
+    jr z, .normalMode
+    ldh [hHeartMode], a
+.normalMode
+    pop af
+    jr .nextState
+
+UpdateCursor::
+.selectPressed
+    xor a, 1
+.updateCursor
+    ldh [hIsMultiplayer], a
+    and a
+    ld a, $10
+    jr z, .skip
+    ld a, $60               ; TODO
+.skip
+    ld [wOAMBuffer + 1], a  ; X coordinate
+    ret
+
+.rightPressed
+    and a
+    ret nz
+    xor a
+    jr .selectPressed
+
+.leftPressed
+    and a
+    ret z
+.initCursor
+    xor a
+    jr .updateCursor
+
+
+; called every frame, serves to exit the demo if it ended or start is pressed
+Call_50C::
+    ldh a, [hDemoNumber]
+    and a
+    ret z
+    call DelayMillisecond
+    xor a
+    ldh [rSB], a
+    ld a, $80
+    ldh [rSC], a
+    ldh a, [hJoyPressed]
+    bit 3, a            ; TODO Start button
+    jr z, .startNotPressed
+    ld a, $33
+    ldh [rSB], a
+    ld a, $81
+    ldh [rSC], a
+    ld a, $06
+    ldh [hGameState], a
+    ret
+
+.startNotPressed
+    ld hl, $FFB0        ; Number of blocks played in demos or multiplayer TODO
+    ldh a, [hDemoNumber]
+    cp a, $02
+    ld b, $10
+    jr z, .skip
+    ld b, $1D
+.skip
+    ld a, [hl]
+    cp b
+    ret nz
+    ld a, $06
+    ldh [hGameState], a
+    ret
+
+INCBIN "baserom.gb", $542, $A98 - $542
+
+; Todo name, TODO has something to do with serial communication?
+DelayMillisecond::
+    push bc
+    ld b, $FA           ; Spin 240 times 4 cycli, about a millisecond
+.loop
+    ld b, b
+    dec b
+    jr nz, .loop
+    pop bc
+    ret
+
+INCBIN "baserom.gb", $AA1, $1444 - $AA1
+
+GameState_08::
+    ld a, IEF_VBLANK
+    ldh [rIE], a
+    xor a
+    ldh [rSB], a
+    ldh [rSC], a
+    ldh [rIF], a
+    call DisableLCD
+    call Call_27AD
+    ld de, $4CD7
+    call LoadTilemap9800
+    call ClearSprites
+    ld hl, $C200
+    ld de, Data_26CF
+    ld c, 2
+    call Call_1776
+    ld de, $C201        ; Metasprite Y-coordinate
+    call PositionMusicTypeMetasprite
+    ldh a, [hGameType]
+    ld e, $12           ; LOW(C212), metasprite X-coordinate
+    ld [de], a
+    inc de
+    cp a, $37
+    ld a, $1C           ; A-Type metasprite
+    jr z, .skip
+    ld a, $1D           ; B-Type metasprite
+.skip
+    ld [de], a
+    call $2671          ; TODO XXX This sets up the metasprites
+    call Call_1517
+    ld a, $D3
+    ldh [rLCDC], a
+    ld a, $0E
+    ldh [hGameState], a
+    ret
+
+PositionMusicTypeMetasprite::
+    ld a, $01           ; Menu selection SFX
+    ld [$DFE0], a       ; TODO I don't think this plays for some reason though
+    ldh a, [hMusicType]
+    push af
+    sub a, $1C          ; The four music types have consecutive metasprite numbers
+    add a
+    ld c, a
+    ld b, $00
+    ld hl, MusicTypeMetaspriteCoordinates
+    add hl, bc
+    ldi a, [hl]
+    ld [de], a
+    inc de
+    ld a, [hl]
+    ld [de], a
+    inc de
+    pop af
+    ld [de], a
+    ret
+
+MusicTypeMetaspriteCoordinates::
+    db $70, $37
+    db $70, $77
+    db $80, $37
+    db $80, $77
+
+INCBIN "baserom.gb", $14B0, $1517 - $14B0
+
+; Set music based on hMusicType?
+Call_1517::
+    ldh a, [hMusicType]
+    sub a, $17          ; Based on metasprite number
+    cp a, $08
+    jr nz, .skip
+    ld a, $FF           ; Disable music
+.skip
+    ld [$DFE8], a
+    ret
+
+INCBIN "baserom.gb", $1525, $1776 - $1525
+
+; Init C metasprites from adresses starting at DE to HL
+Call_1776::
+.loop
+    push hl
+    ld b, 6             ; 6 datapoints per metasprite, but what are they...
+.innerLoop
+    ld a, [de]
+    ldi [hl], a
+    inc de
+    dec b
+    jr nz, .innerLoop
+    pop hl
+    ld a, $10
+    add l
+    ld l, a
+    dec c
+    jr nz, .loop
+    ld [hl], $80        ; Make the next sprite invisible?
+    ret
+
+ClearSprites::
+    xor a
+    ld hl, wOAMBuffer
+    ld b, 4 * 40
+.loop
+    ldi [hl], a
+    dec b
+    jr nz, .loop
+    ret
+
+INCBIN "baserom.gb", $1795, $26CF - $1795
+; First config screen metasprites
+Data_26CF::
+    db $00, $70, $37, $1C, $00, $00
+    db $00, $38, $37, $1C, $00, $00
+
+INCBIN "baserom.gb", $26DB, $2795 - $26DB
+
+ClearTilemap9800::
     ld hl, _SCRN0 + $400 - 1    ; TODO constants
     ld bc, $400
 .loop
-    ld a, $2F           ; TODO charmap
+    ld a, " "
     ldd [hl], a
     dec bc
     ld a, b
@@ -482,7 +908,121 @@ ClearBgMap::
     jr nz, .loop
     ret
 
-INCBIN "baserom.gb", $27A4, $29A6 - $27A4
+; Copy BC bytes from HL to DE
+CopyData::
+.loop
+    ldi a, [hl]
+    ld [de], a
+    inc de
+    dec bc
+    ld a, b
+    or c
+    jr nz, .loop
+    ret
+
+Call_27AD::
+    call LoadFontTileset
+    ld bc, $00A0
+    call CopyData
+    ld hl, $323F
+    ld de, $8300
+    ld bc, $0D00
+    call CopyData
+    ret
+
+LoadFontTileset::
+    ld hl, $415F        ; Todo
+    ld bc, $0138
+    ld de, $8000
+.loop
+    ldi a, [hl]
+    ld [de], a
+    inc de
+    ld [de], a          ; Expand 1BPP image into 2BPP
+    inc de
+    dec bc
+    ld a, b
+    or c
+    jr nz, .loop
+    ret
+
+LoadCopyrightScreenTileset::
+    call LoadFontTileset
+    ld bc, $0DA0        ; Bug? This seems way too much, copying garbage data
+    call CopyData
+    ret
+
+Call_27E1::
+    ld bc, $1000
+    ld de, $8000
+    call CopyData
+    ret
+
+; Todo name
+LoadTilemap9800::
+    ld hl, $9800
+    ld b, SCRN_Y_B
+.loop
+    push hl
+    ld c, SCRN_X_B
+.innerLoop
+    ld a, [de]
+    ldi [hl], a
+    inc de
+    dec c
+    jr nz, .innerLoop
+    pop hl
+    push de
+    ld de, SCRN_VX_B
+    add hl, de
+    pop de
+    dec b
+    jr nz, .loop
+    ret
+
+Call_2804::
+    ld b, $0A
+    push hl
+.loop
+    ld a, [de]
+    cp a, $FF
+    jr z, .skip
+    ldi [hl], a
+    inc de
+    dec b
+    jr nz, .loop
+    pop hl
+    push de
+    ld de, $0020
+    add hl, de
+    pop de
+    jr Call_2804
+
+.skip
+    pop hl
+    ld a, $02
+    ldh [$E3], a
+    ret
+
+; Disabling the LCD *must* be performed during VBlank only, to prevent damaging
+; the hardware
+DisableLCD::
+    ldh a, [rIE]
+    ldh [$A1], a
+    res 0, a            ; Bit 0 is VBlank
+    ldh [rIE], a
+.wait
+    ldh a, [rLY]
+    cp a, $91           ; TODO
+    jr nz, .wait
+    ldh a, [rLCDC]
+    and a, $FF ^ LCDCF_ON
+    ldh [rLCDC], a
+    ldh a, [$A1]
+    ldh [rIE], a
+    ret
+
+INCBIN "baserom.gb", $2839, $29A6 - $2839
 
 ReadJoypad::
     ld a, P1F_GET_DPAD
@@ -513,10 +1053,123 @@ ENDR
     ldh [rP1], a
     ret
 
-INCBIN "baserom.gb", $29E3, $2A7F - $29E3
+Call_29E3::
+    ldh a, [$FFB2]
+    sub a, $10
+    srl a
+    srl a
+    srl a
+    ld de, $0000
+    ld e, a
+    ld hl, $9800        ; TODO
+    ld b, $20
+.loop
+    add hl, de
+    dec b
+    jr nz, .loop
+    ldh a, [$B3]
+    sub a, $08
+    srl a
+    srl a
+    srl a
+    ld de, $0000
+    ld e, a
+    add hl, de
+    ld a, h
+    ldh [$FFB5], a
+    ld a, l
+    ldh [$FFB4], a
+    ret
+
+; This exactly mirrors some curious code in Super Mario Land
+; TODO 
+; Unused?
+Call_2A10::
+    ldh a, [$B5]
+    ld d, a
+    ldh a, [$B4]
+    ld e, a
+    ld b, $04
+.loop
+    rr d
+    rr e
+    dec b
+    jr nz, .loop
+    ld a, e
+    sub a, $84
+    and a, $FE
+    rlca
+    rlca
+    add a, $08
+    ldh [$B2], a
+    ldh a, [$B4]
+    and a, $1F
+    rla
+    rla
+    rla
+    add a, $08
+    ldh [$B3], a
+    ret
+
+; Draw the number at DE to the tilemap at HL
+; Here, FFE0 doubles as a Boolean indicating the number has started
+PrintScore::
+    ldh a, [$E0]
+    and a
+    ret z
+    ld c, 3             ; Maximum 3 number pairs
+    xor a
+    ldh [$E0], a
+.printDigitPair
+    ld a, [de]
+    ld b, a
+    swap a
+    and a, $0F          ; More significant digit of the pair
+    jr nz, .startNumber1
+    ldh a, [$E0]        ; If zero, check if the number has already started
+    and a
+    ld a, "0"           ; Print a zero if it has started
+    jr nz, .printFirstDigit
+    ld a, " "           ; Otherwise, print a space
+.printFirstDigit
+    ldi [hl], a
+    ld a, b
+    and a, $0F          ; Again for the lesser significant digit
+    jr nz, .startNumber2
+    ldh a, [$E0]
+    and a
+    ld a, "0"
+    jr nz, .printSecondDigit
+    ld a, 1             ; If we're at the ones, print a zero even if the number
+    cp c                ; hasn't started yet
+    ld a, "0"
+    jr z, .printSecondDigit
+    ld a, " "
+.printSecondDigit
+    ldi [hl], a
+    dec e
+    dec c               ; Next (less significant) digit pair
+    jr nz, .printDigitPair
+    xor a
+    ldh [$E0], a
+    ret
+
+.startNumber1
+    push af
+    ld a, 1
+    ldh [$E0], a
+    pop af
+    jr .printFirstDigit
+
+.startNumber2
+    push af
+    ld a, 1
+    ldh [$E0], a
+    pop af
+    jr .printSecondDigit
 
 ; TODO Use rgbds's LOAD block
-DMARoutine:
+DMARoutine::
     ld a, HIGH(wOAMBuffer)
     ldh [rDMA], a
     ld a, $28
@@ -527,6 +1180,29 @@ DMARoutine:
 DMARoutineEnd:
 
 INCBIN "baserom.gb", $2A89, $4000 - $2A89
-INCBIN "baserom.gb", $4000, $8000 - $4000
+INCBIN "baserom.gb", $4000, $4A07 - $4000
+
+; TODO
+CopyrightScreenTilemap::
+db "                    "
+db "”tm and ©1987 elorg,"
+db " tetris licensed to "
+db "    bullet-proof    "
+db "    software and    "
+db "   sub-licensed to  "
+db "      nintendo.     "
+db "                    "
+db " ©1989 bullet-proof "
+db "      software.     "
+db "   ©", $30, $31, $32, $31, " ", $34, $35, $36, $37, $38, $39, "     "
+db "                    "
+db "all rights reserved."
+db "                    "
+db "  original concept, "
+db " design and program "
+db "by alexey pazhitnov.”"
+db "                    "
+
+INCBIN "baserom.gb", $4B6F, $8000 - $4B6F
 
 ; vim: set expandtab tabstop=4 shiftwidth=4 
