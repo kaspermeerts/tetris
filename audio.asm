bd66df06121430565a1076988a190632b3a82e08
Kasper Meerts <kaspermeerts@gmail.com>
2022-02-20 19:30:39 +0100

    Uncommented disassembly of audio code

--- audio.asm
+++ audio.asm
@@ -83,9 +83,9 @@ _UpdateAudio::
     call PlaySquareSFX
     call PlayNoiseSFX
     call PlayWaveSFX
-    call $6A21
-    call $6C44
-    call $6A65
+    call Call_6A21
+    call PlayMusic
+    call Call_6A65
 .out
     xor a
     ld [$DFE0], a
@@ -100,7 +100,7 @@ _UpdateAudio::
     ret
 
 .pauseAudio
-    call $69C7
+    call _InitAudio.muteChannels
     xor a
     ld [$DFE1], a
     ld [$DFF1], a
@@ -809,6 +809,7 @@ _InitAudio::
     ldh [rNR51], a      ; Output all channels to both terminals
     ld a, $03
     ld [$DF78], a
+.muteChannels
     ld a, $08           ; On channels with a volume envelope, set volume to 
     ldh [rNR12], a      ; zero and envelope to increase (fade-in?)
     ldh [rNR22], a
@@ -873,7 +874,736 @@ _Unused::
     call _InitAudio
     ret
 
-INCBIN "baserom.gb", $6A21, $6EA9 - $6A21
+Call_6A21::
+    ld hl, $DFE8
+    ldi a, [hl]
+    and a
+    ret z
+    cp a, $FF
+    jr z, _Unused
+    ld [hl], a
+    ld b, a
+    ld hl, MusicPointers
+    and a, $1F          ; Curious, there are only $11 songs?
+    call LookupSFXPointer.lookup
+    call Call_6B13
+    call Call_6A3C
+    ret
+
+Call_6A3C:
+    ld a, [$DFE9]
+    and a
+    ret z
+    ld hl, Data_6ABE
+.loop
+    dec a
+    jr z, .jp_6A4D
+    inc hl
+    inc hl
+    inc hl
+    inc hl
+    jr .loop
+
+.jp_6A4D
+    ldi a, [hl]
+    ld [$DF78], a
+    ldi a, [hl]
+    ld [$DF76], a
+    ldi a, [hl]
+    ld [$DF79], a
+    ldi a, [hl]
+    ld [$DF7A], a
+    xor a
+    ld [$DF75], a
+    ld [$DF77], a
+    ret
+
+Call_6A65::
+    ld a, [$DFE9]
+    and a
+    jr z, .label_6AA8
+    ld hl, $DF75
+    ld a, [$DF78]
+    cp a, $01
+    jr z, .label_6AAC
+    cp a, $03
+    jr z, .label_6AA8
+    inc [hl]
+    ldi a, [hl]
+    cp [hl]
+    jr nz, .label_6AB1
+    dec l
+    ld [hl], $00
+    inc l
+    inc l
+    inc [hl]
+    ld a, [$DF79]
+    bit 0, [hl]
+    jp z, .label_6A8F
+    ld a, [$DF7A]
+.label_6A8F
+    ld b, a
+    ld a, [$DFF1]
+    and a
+    jr z, .label_6A9A
+    set 2, b
+    set 6, b
+.label_6A9A
+    ld a, [$DFF9]
+    and a
+    jr z, .label_6AA4
+    set 3, b
+    set 7, b
+.label_6AA4
+    ld a, b
+.label_6AA5
+    ldh [rNR51], a
+    ret
+
+.label_6AA8
+    ld a, $FF
+    jr .label_6AA5
+
+.label_6AAC
+    ld a, [$DF79]
+    jr .label_6A8F
+
+.label_6AB1
+    ld a, [$DFF9]
+    and a
+    jr nz, .label_6AA8
+    ld a, [$DFF1]
+    and a
+    jr nz, .label_6AA8
+    ret
+
+Data_6ABE::
+    db $01, $24, $EF, $56
+    db $01, $00, $E5, $00
+    db $01, $20, $FD, $00
+    db $01, $20, $DE, $F7
+    db $03, $18, $7F, $F7
+    db $03, $18, $F7, $7F
+    db $03, $48, $DF, $5B
+    db $01, $18, $DB, $E7
+    db $01, $00, $FD, $F7
+    db $03, $20, $7F, $F7
+    db $01, $20, $ED, $F7
+    db $01, $20, $ED, $F7
+    db $01, $20, $ED, $F7
+    db $01, $20, $ED, $F7
+    db $01, $20, $ED, $F7
+    db $01, $20, $EF, $F7
+    db $01, $20, $EF, $F7
+
+CopyPointerIndirect::
+    ldi a, [hl]
+    ld c, a
+    ld a, [hl]
+    ld b, a
+    ld a, [bc]
+    ld [de], a
+    inc e               ; XXX Should this be INC DE?                           
+    inc bc
+    ld a, [bc]
+    ld [de], a
+    ret
+
+CopyPointer::
+    ldi a, [hl]
+    ld [de], a
+    inc e               ; XXX Idem
+    ldi a, [hl]
+    ld [de], a
+    ret
+
+Call_6B13::
+    call _InitAudio.muteChannels
+    xor a
+    ld [$DF75], a
+    ld [$DF77], a
+    ld de, $DF80
+    ld b, $00
+    ldi a, [hl]
+    ld [de], a
+    inc e
+    call CopyPointer
+    ld de, $DF90
+    call CopyPointer
+    ld de, $DFA0
+    call CopyPointer
+    ld de, $DFB0
+    call CopyPointer
+    ld de, $DFC0
+    call CopyPointer
+    ld hl, $DF90
+    ld de, $DF94
+    call CopyPointerIndirect
+    ld hl, $DFA0
+    ld de, $DFA4
+    call CopyPointerIndirect
+    ld hl, $DFB0
+    ld de, $DFB4
+    call CopyPointerIndirect
+    ld hl, $DFC0
+    ld de, $DFC4
+    call CopyPointerIndirect
+    ld bc, $0410
+    ld hl, $DF92
+.loop
+    ld [hl], $01
+    ld a, c
+    add l
+    ld l, a
+    dec b
+    jr nz, .loop
+    xor a
+    ld [$DF9E], a
+    ld [$DFAE], a
+    ld [$DFBE], a
+    ret
+
+Call_6B7D::
+.label_6B7D
+    push hl
+    xor a
+    ldh  [rNR30], a
+    ld l, e
+    ld h, d
+    call LoadWavePattern
+    pop hl
+    jr .label_6BB3
+
+.label_6B89
+    call IncrementPointer
+    call LoadFromHLindirect
+    ld e, a
+    call IncrementPointer
+    call LoadFromHLindirect
+    ld d, a
+    call IncrementPointer
+    call LoadFromHLindirect
+    ld c, a
+    inc l
+    inc l
+    ld [hl], e
+    inc l
+    ld [hl], d
+    inc l
+    ld [hl], c
+    dec l
+    dec l
+    dec l
+    dec l
+    push hl
+    ld hl, $DF70
+    ld a, [hl]
+    pop hl
+    cp a, $03
+    jr z, .label_6B7D
+.label_6BB3
+    call IncrementPointer
+    jp $6C5E
+
+IncrementPointer::
+    push de
+    ldi a, [hl]
+    ld e, a
+    ldd a, [hl]
+    ld d, a
+    inc de
+.storeDE
+    ld a, e
+    ldi [hl], a
+    ld a, d
+    ldd [hl], a
+    pop de
+    ret
+
+IncrementPointerTwice::
+    push de
+    ldi a, [hl]
+    ld e, a
+    ldd a, [hl]
+    ld d, a
+    inc de              ; Instead of incrementing DE twice, could've just
+    inc de              ; incremented once, and jumped one byte earlier. Bug?
+    jr IncrementPointer.storeDE
+
+; A ← [[HL]]
+LoadFromHLindirect::
+    ldi a, [hl]
+    ld c, a
+    ldd a, [hl]
+    ld b, a
+    ld a, [bc]
+    ld b, a             ; Why? Bug?
+    ret
+
+Jmp_6BD5::
+    pop hl
+    jr .label_6C04
+
+.label_6BD8
+    ld a, [$DF70]
+.label_6BDB
+    cp a, $03
+    jr nz, .label_6BEF
+    ld a, [$DFB8]
+    bit 7, a
+    jr z, .label_6BEF
+    ld a, [hl]
+    cp a, $06
+    jr nz, .label_6BEF
+    ld a, $40
+    ldh [rNR32], a
+.label_6BEF
+    push hl
+    ld a, l
+    add a, $09
+    ld l, a
+    ld a, [hl]
+    and a
+    jr nz, Jmp_6BD5
+    ld a, l
+    add a, $04
+    ld l, a
+    bit 7, [hl]
+    jr nz, Jmp_6BD5
+    pop hl
+    call $6D67
+.label_6C04
+    dec l
+    dec l
+    jp $6D39
+
+.label_6C09
+    dec l
+    dec l
+    dec l
+    dec l
+    call IncrementPointerTwice
+.label_6C10
+    ld a, l
+    add a, $04
+    ld e, a
+    ld d, h
+    call CopyPointerIndirect
+    cp a, $00
+    jr z, .label_6C3B
+    cp a, $FF
+    jr z, .label_6C24
+    inc l
+    jp $6C5C
+
+.label_6C24
+    dec l
+    push hl
+    call IncrementPointerTwice
+    call LoadFromHLindirect
+    ld e, a
+    call IncrementPointer
+    call LoadFromHLindirect
+    ld d, a
+    pop hl
+    ld a, e
+    ldi [hl], a
+    ld a, d
+    ldd [hl], a
+    jr .label_6C10
+
+.label_6C3B
+    ld hl, $DFE9
+    ld [hl], $00
+    call _InitAudio
+    ret
+
+PlayMusic::
+    ld hl, $DFE9
+    ld a, [hl]
+    and a
+    ret z
+    ld a, $01
+    ld [$DF70], a
+    ld hl, $DF90
+.label_6C52
+    inc l
+    ldi a, [hl]
+    and a
+    jp z, Jmp_6BD5.label_6C04
+    dec [hl]
+    jp nz, Jmp_6BD5.label_6BD8
+    inc l
+    inc l
+    call LoadFromHLindirect
+    cp a, $00
+    jp z, Jmp_6BD5.label_6C09
+    cp a, $9D
+    jp z, Call_6B7D.label_6B89
+    and a, $F0
+    cp a, $A0
+    jr nz, .label_6C8B
+    ld a, b
+    and a, $0F
+    ld c, a
+    ld b, $00
+    push hl
+    ld de, $DF81
+    ld a, [de]
+    ld l, a
+    inc de
+    ld a, [de]
+    ld h, a
+    add hl, bc
+    ld a, [hl]
+    pop hl
+    dec l
+    ldi [hl], a
+    call IncrementPointer
+    call LoadFromHLindirect
+.label_6C8B
+    ld a, b
+    ld c, a
+    ld b, $00
+    call IncrementPointer
+    ld a, [$DF70]
+    cp a, $04
+    jp z, .label_6CBC
+    push hl
+.label_6C9B
+    ld a, l
+    add a, $05
+    ld l, a
+    ld e, l
+    ld d, h
+    inc l
+    inc l
+    ld a, c
+    cp a, $01
+    jr z, .label_6CB7
+    ld [hl], $00
+    ld hl, NotePitches
+    add hl, bc
+    ldi a, [hl]
+    ld [de], a
+    inc e
+    ld a, [hl]
+    ld [de], a
+    pop hl
+    jp .label_6CD3
+
+.label_6CB7
+    ld [hl], $01
+    pop hl
+    jr .label_6CD3
+
+.label_6CBC
+    push hl
+    ld de, $DFC6
+    ld hl, Data_6E94
+    add hl, bc
+.label_6CC4
+    ldi a, [hl]
+    ld [de], a
+    inc e
+    ld a, e
+    cp a, $CB
+    jr nz, .label_6CC4
+    ld c, $20
+    ld hl, $DFC4
+    jr .label_6D01
+
+.label_6CD3
+    push hl
+    ld a, [$DF70]
+    cp a, $01
+    jr z, .label_6CFC
+    cp a, $02
+    jr z, .label_6CF8
+    ld c, $1A
+    ld a, [$DFBF]
+    bit 7, a
+    jr nz, .label_6CED
+    xor a
+    ldh [c], a
+    ld a, $80
+    ldh [c], a
+.label_6CED
+    inc c
+    inc l
+    inc l
+    inc l
+    inc l
+    ldi a, [hl]
+    ld e, a
+    ld d, $00
+    jr .label_6D0D
+
+.label_6CF8
+    ld c, $16
+    jr .label_6D01
+
+.label_6CFC
+    ld c, $10
+    ld a, $00
+    inc c
+.label_6D01
+    inc l
+    inc l
+    inc l
+    ldd a, [hl]
+    and a
+    jr nz, .label_6D57
+    ldi a, [hl]
+    ld e, a
+.label_6D0A
+    inc l
+    ldi a, [hl]
+    ld d, a
+.label_6D0D
+    push hl
+    inc l
+    inc l
+    ldi a, [hl]
+    and a
+    jr z, .label_6D16
+    ld e, $01
+.label_6D16
+    inc l
+    inc l
+    ld [hl], $00
+    inc l
+    ld a, [hl]
+    pop hl
+    bit 7, a
+    jr nz, .label_6D34
+    ld a, d
+    ldh [c], a
+    inc c
+    ld a, e
+    ldh [c], a
+    inc c
+    ldi a, [hl]
+    ldh [c], a
+    inc c
+    ld a, [hl]
+    or a, $80
+    ldh [c], a
+    ld a, l
+    or a, $05
+    ld l, a
+    res 0, [hl]
+.label_6D34
+    pop hl
+    dec l
+    ldd a, [hl]
+    ldd [hl], a
+    dec l
+    ld de, $DF70
+    ld a, [de]
+    cp a, $04
+    jr z, .label_6D4A
+    inc a
+    ld [de], a
+    ld de, $0010
+    add hl, de
+    jp .label_6C52
+
+.label_6D4A
+    ld hl, $DF9E
+    inc [hl]
+    ld hl, $DFAE
+    inc [hl]
+    ld hl, $DFBE
+    inc [hl]
+    ret
+
+.label_6D57
+    ld b, $00
+    push hl
+    pop hl              ; Hm?
+    inc l
+    jr .label_6D0A
+
+.label_6D5E
+    ld a, b
+    srl a
+    ld l, a
+    ld h, $00
+    add hl, de
+    ld e, [hl]
+    ret
+
+Call_6D67::
+    push hl
+    ld a, l
+    add a, $06
+    ld l, a
+    ld a, [hl]
+    and a, $0F
+    jr z, .label_6D89
+    ld [$DF71], a
+    ld a, [$DF70]
+    ld c, $13
+    cp a, $01
+    jr z, .label_6D8B
+    ld c, $18
+    cp a, $02
+    jr z, .label_6D8B
+    ld c, $1D
+    cp a, $03
+    jr z, .label_6D8B
+.label_6D89
+    pop hl
+    ret
+
+.label_6D8B
+    inc l
+    ldi a, [hl]
+    ld e, a
+    ld a, [hl]
+    ld d, a
+    push de
+    ld a, l
+    add a, $04
+    ld l, a
+    ld b, [hl]
+    ld a, [$DF71]
+    cp a, $01
+    jr .label_6DA6      ; Huh? Bug?
+
+.label_6D9D
+    cp a, $03
+    jr .label_6DA1
+
+.label_6DA1
+    ld hl, $FFFF
+    jr .label_6DC2
+
+.label_6DA6
+    ld de, Data_6DCB
+    call $6D5E
+    bit 0, b
+    jr nz, .label_6DB2
+    swap e
+.label_6DB2
+    ld a, e
+    and a, $0F
+    bit 3, a
+    jr z, .label_6DBF
+    ld h, $FF
+    or a, $F0
+    jr .label_6DC1
+
+.label_6DBF
+    ld h, $00
+.label_6DC1
+    ld l, a
+.label_6DC2
+    pop de
+    add hl, de
+    ld a, l
+    ldh [c], a
+    inc c
+    ld a, h
+    ldh [c], a
+    jr .label_6D89
+
+Data_6DCB::
+    db $00, $00, $00, $00, $00
+    db $00, $10, $00, $0F, $00
+    db $00, $11, $00, $0F, $F0
+    db $01, $12, $10, $FF, $EF
+    db $01, $12, $10, $FF, $EF
+    db $01, $12, $10, $FF, $EF
+    db $01, $12, $10, $FF, $EF
+    db $01, $12, $10, $FF, $EF
+    db $01, $12, $10, $FF, $EF
+    db $01, $12, $10, $FF, $EF
+    db $01, $12, $10, $FF, $EF
+
+NotePitches::
+    dw $F00 ; 
+    dw $02C ;   65.4 Hz C 2 (-0)
+    dw $09C ;   69.3 Hz C#2 (-0) Bug! 09D is closer
+    dw $106 ;   73.4 Hz D 2 (-1) Bug! 107 is closer
+    dw $16B ;   77.8 Hz D#2 (+0)
+    dw $1C9 ;   82.4 Hz E 2 (-0)
+    dw $223 ;   87.3 Hz F 2 (+0)
+    dw $277 ;   92.5 Hz F#2 (+0)
+    dw $2C6 ;   98.0 Hz G 2 (-1) Bug! 2C7 is closer
+    dw $312 ;  103.9 Hz G#2 (+1)
+    dw $356 ;  109.8 Hz A 2 (-4) Bug! 357 is closer
+    dw $39B ;  116.5 Hz A#2 (-0)
+    dw $3DA ;  123.4 Hz B 2 (-1)
+    dw $416 ;  130.8 Hz C 3 (-0)
+    dw $44E ;  138.6 Hz C#3 (-0)
+    dw $483 ;  146.8 Hz D 3 (-1)
+    dw $4B5 ;  155.5 Hz D#3 (-1)
+    dw $4E5 ;  164.9 Hz E 3 (+1)
+    dw $511 ;  174.5 Hz F 3 (-1)
+    dw $53B ;  184.9 Hz F#3 (-1)
+    dw $563 ;  195.9 Hz G 3 (-1)
+    dw $589 ;  207.7 Hz G#3 (+1)
+    dw $5AC ;  219.9 Hz A 3 (-1)
+    dw $5CE ;  233.2 Hz A#3 (+1)
+    dw $5ED ;  246.8 Hz B 3 (-1)
+    dw $60A ;  261.1 Hz C 4 (-3) Bug! 60B is closer
+    dw $627 ;  277.1 Hz C#4 (-0)
+    dw $642 ;  293.9 Hz D 4 (+1)
+    dw $65B ;  311.3 Hz D#4 (+1)
+    dw $672 ;  329.3 Hz E 4 (-2)
+    dw $689 ;  349.5 Hz F 4 (+1)
+    dw $69E ;  370.3 Hz F#4 (+1)
+    dw $6B2 ;  392.4 Hz G 4 (+2)
+    dw $6C4 ;  414.8 Hz G#4 (-2)
+    dw $6D6 ;  439.8 Hz A 4 (-1)
+    dw $6E7 ;  466.4 Hz A#4 (+1)
+    dw $6F7 ;  494.6 Hz B 4 (+3)
+    dw $706 ;  524.3 Hz C 5 (+3)
+    dw $714 ;  555.4 Hz C#5 (+3)
+    dw $721 ;  587.8 Hz D 5 (+1)
+    dw $72D ;  621.2 Hz D#5 (-3)
+    dw $739 ;  658.7 Hz E 5 (-2)
+    dw $744 ;  697.2 Hz F 5 (-3)
+    dw $74F ;  740.5 Hz F#5 (+1)
+    dw $759 ;  784.9 Hz G 5 (+2)
+    dw $762 ;  829.6 Hz G#5 (-2)
+    dw $76B ;  879.7 Hz A 5 (-1)
+    dw $773 ;  929.6 Hz A#5 (-5)
+    dw $77B ;  985.5 Hz B 5 (-4)
+    dw $783 ; 1048.6 Hz C 6 (+3)
+    dw $78A ; 1110.8 Hz C#6 (+3)
+    dw $790 ; 1170.3 Hz D 6 (-6)
+    dw $797 ; 1248.3 Hz D#6 (+5)
+    dw $79D ; 1324.0 Hz E 6 (+7)
+    dw $7A2 ; 1394.4 Hz F 6 (-3)
+    dw $7A7 ; 1472.7 Hz F#6 (-9)
+    dw $7AC ; 1560.4 Hz G 6 (-8)
+    dw $7B1 ; 1659.1 Hz G#6 (-2)
+    dw $7B6 ; 1771.2 Hz A 6 (+11)
+    dw $7BA ; 1872.5 Hz A#6 (+7)
+    dw $7BE ; 1985.9 Hz B 6 (+9)
+    dw $7C1 ; 2080.5 Hz C 7 (-10)
+    dw $7C4 ; 2184.5 Hz C#7 (-26) Bug! 7C5 is closer
+    dw $7C8 ; 2340.6 Hz D 7 (-6)
+    dw $7CB ; 2473.1 Hz D#7 (-11)
+    dw $7CE ; 2621.4 Hz E 7 (-10)
+    dw $7D1 ; 2788.8 Hz F 7 (-3)
+    dw $7D4 ; 2978.9 Hz F#7 (+11)
+    dw $7D6 ; 3120.8 Hz G 7 (-8)
+    dw $7D9 ; 3360.8 Hz G#7 (+20)
+    dw $7DB ; 3542.5 Hz A 7 (+11)
+    dw $7DD ; 3744.9 Hz A#7 (+7)
+    dw $7DF ; 3971.9 Hz B 7 (+9)
+
+Data_6E94::
+    db $00
+    db $00, $00, $00, $00, $C0
+    db $A1, $00, $3A, $00, $C0
+    db $B1, $00, $29, $01, $C0
+    db $61, $00, $3A, $00, $C0
+
 ;             ▄▀▄       ▄        
 ;           ▄▀                   
 ;         ▄▀        ▄▀ ▀ ▄       
@@ -937,3 +1667,6 @@ UpdateAudio::
     jp _UpdateAudio
 InitAudio::
     jp _InitAudio
+
+
+; vim: set expandtab tabstop=4 shiftwidth=4 
